e1fcb404249a614499ad32ce7e7de00e965237cc
Mtwod <maximewong@hotmail.com>
2020-11-23 17:50:44 +0100

    edited users-carts relation in migration, added 'has_many' to MODEL User - LiveShare with AR

--- db/schema.rb
+++ db/schema.rb
@@ -58,5 +58,4 @@ ActiveRecord::Schema.define(version: 2020_11_23_163922) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "carts", "users"
 end
