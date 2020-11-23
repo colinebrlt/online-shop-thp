8b1ad6c32b270c033bb2f55815b128c6f28c0781
Rudyar <arthur.rudy.code@gmail.com>
2020-11-23 14:59:12 +0100

    devise users + views + model validations

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable, :confirmable
+
+  validates_presence_of :first_name, :last_name
+end
