76d6245aca1c1790d0bc4f267cbc2b31b93ec4e0
Mtwod <maximewong@hotmail.com>
2020-11-24 12:20:54 +0100

    add CONTROLLER Carts, and edited ROUTES to only show, update, destroy.

---
+++ app/controllers/carts_controller.rb
@@ -0,0 +1,10 @@
+class CartsController < ApplicationController
+  def show
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
   
+  get 'carts/show'
+  get 'carts/update'
+  get 'carts/destroy'
   devise_for :users
   
   root 'items#index'
   resources :items
   resources :users
-  resources :carts
+  resources :carts, only: [:show, :update, :destroy]
   resources :line_items
 end
