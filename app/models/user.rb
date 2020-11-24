80ca7bc046ba0b3838c43d5bd6abc27d2d2651e2
vanak <ven2kaze@gmail.com>
2020-11-24 13:46:50 +0100

    connexion with MailJet added, UserMailer created, message when user creating done, confirmation_instructions and email_changed via users/mailer updated, navbar updated

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,15 @@ class User < ApplicationRecord
          
   has_many :carts
   has_many :orders
+
+  after_create :welcome_send
+
+  def welcome_send
+    UserMailer.welcome_email(self).deliver_now
+  end
+
+  def has_first_name?
+    self.first_name
+  end
+
 end
