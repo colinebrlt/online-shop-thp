c4b05821f674c8486553ae8e104c200abad95916
Coline Brault <coline.brault@gmail.com>
2020-11-24 10:11:21 +0100

    user show and user edit page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,34 @@
 class UsersController < ApplicationController
+  before_action :check_user
 
   def show
     @user = User.find(params[:id])
   end 
+
+  def edit
+  end 
+
+  def update
+    if @user.update(user_params)
+      redirect_to user_path, success: "Vos informations ont bien été mises à jour"
+    else
+      render :edit
+    end
+  end 
+
+  def destroy
+    @user.destroy
+    redirect_to @user, success: "Votre profil a bien été supprimé"
+  end 
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name)
+  end
+
+  def check_user
+    @user = User.find(params[:id])
+  end
 
 end
