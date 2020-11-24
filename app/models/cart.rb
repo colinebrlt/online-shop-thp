ceac0ee9727b6309afc9a426a1ee3db6d18f9a4e
Mtwod <maximewong@hotmail.com>
2020-11-24 12:10:57 +0100

    add instance method to MODEL Cart : total_price

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,4 +1,12 @@
 class Cart < ApplicationRecord
   belongs_to :user
   has_many :line_items
+
+  def total_price
+    total = 0
+    self.line_items.each do |line_item|
+      total += line_item.item.price * line_item.quantity
+    end
+    return total
+  end
 end
