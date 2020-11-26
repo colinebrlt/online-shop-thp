80dc009782f7682c292b680b50b4fd3506b1a947
Mtwod <maximewong@hotmail.com>
2020-11-26 18:19:29 +0100

    added check conditions for author of cart, order and user profile; edited VIEW orders/show (added buttons), user/_order and /show

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ class ApplicationController < ActionController::Base
   add_flash_types :success, :info, :warning, :danger, :error, :notice, :alert
   include ApplicationHelper
   include CurrentCart
+
+  def redirect_if_user_not_admin
+    unless current_user.is_admin
+      redirect_to root_path, danger: "Vous n'êtes pas administrateur de ce site !"
+    end
+  end
 end
