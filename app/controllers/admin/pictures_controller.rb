645f52c0d52791337f590c442ee3f0a4f2091010
Rudyar <arthur.rudy.code@gmail.com>
2020-11-26 17:09:39 +0100

    delete pictures_controller

--- app/controllers/admin/pictures_controller.rb
+++
@@ -1,8 +0,0 @@
-class Admin::PicturesController < Admin::FacadeController
-
-  def create
-    @item = User.find(params[:item_id])
-    @item.picture.attach(params[:picture])
-    redirect_to(root_path)
-  end
-end
